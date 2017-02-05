827bdae19e4ccf5c849434f45b4756ffdc466e59
Kay Wolter <kay.wolter@codeva.de>
2017-02-05 18:04:15 +0100

    Added view engine

--- server/boot/authentication.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-module.exports = function enableAuthentication(server) {
-    server.enableAuth();
-};
-//# sourceMappingURL=authentication.js.map
--- server/boot/root.ts
+++ server/boot/root.ts
@@ -1,8 +1,14 @@
-'use strict';
+import * as path from 'path';
 
 module.exports = function(server) {
+  server.set('view engine', 'pug');
+  server.set('views', path.join(process.cwd(), 'views'));
+
   // Install a `/` route that returns server status
   var router = server.loopback.Router();
-  router.get('/', server.loopback.status());
+  router.get('/', (req, res) => {
+    const ctx = {};
+    res.render('index', ctx);
+  });
   server.use(router);
 };
